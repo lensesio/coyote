72f723b64e4d0f29579bac887728ca4ffc827ecf
Gerasimos Maropoulos <kataras2006@hotmail.com>
2018-05-23 18:43:15 +0300

    Add a test case for `OutFilter#NoRegex`

---
+++ entry_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestOutFilterNoRegex(t *testing.T) {
+	hasAndExpected := `{"categories":{"Infrastructure":[{"id":1,"description":"License is invalid","category":"Infrastructure","enabled":true,"isAvailable":true}]}}\n`
+
+	entry := Entry{
+		Stdout: OutFilters{
+			OutFilter{
+				Match:   []string{hasAndExpected},
+				NoRegex: true,
+			},
+		},
+	}
+
+	ok, err := entry.Test(hasAndExpected, "")
+	if err != nil {
+		if ok {
+			t.Fatalf("expected to not be passed if error")
+		}
+		t.Error(err)
+	}
+
+	if !ok && err == nil {
+		t.Fatalf("expected to be passed if error is nil")
+	}
+}
